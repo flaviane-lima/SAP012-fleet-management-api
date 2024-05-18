ff5e43516faa96b8e8dd05d4191c15cfe723d3c6
flaviane-lima <rflaviane41@gmail.com>
2024-05-17 23:23:11 -0300

    além de escrever as funções coloquei alguns console.log para saber o que estava recebendo da função

--- controller/taxi.js
+++ controller/taxi.js
@@ -5,6 +5,7 @@ const taxiService = require('../services/taxiServices');
     const paginacao = parseInt(req.query.page);
     try {
       const taxis = await taxiService.getTaxis(paginacao);
+      console.log(taxis);
       resp.status(200).json(taxis);
     } catch (error) {
       resp.status(500).json({ error: error.message });
@@ -13,15 +14,28 @@ const taxiService = require('../services/taxiServices');
     };
 
     exports.getTaxisIdTrajectorie = async (req, resp) => {
-      const id = req.params.id;
+      const id = parseInt(req.params.id, 10);
       const date = req.params.date;
       const paginacao = parseInt(req.query.page);
       try {
         const taxiTrajectorie = await taxiService.getTaxisIdTrajectorie(id, date, paginacao);
+        console.log(taxiTrajectorie);
         resp.status(200).json(taxiTrajectorie);
       } catch (error) {
         resp.status(500).json({ error: error.message });
       }
     };
+
+    exports.getTaxisLocalizacao = async (req, resp) => {
+      const paginacao = parseInt(req.query.page);
+      try {
+        const taxisUltimaLocalizacao = await taxiService.getTaxisLocalizacao(paginacao);
+        console.log(taxisUltimaLocalizacao);
+        resp.status(200).json(taxisUltimaLocalizacao);
+      } catch (error) {
+        resp.status(500).json({ error: error.message });
+      }
+  
+      };
 
 
