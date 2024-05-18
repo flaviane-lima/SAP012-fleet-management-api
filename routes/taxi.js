e9a6886c2df1bf3cc66cd8725c07ef59cc67841b
flaviane-lima <rflaviane41@gmail.com>
2024-05-17 23:24:56 -0300

    acrescentei os endpoint de rota da getTaxisTrajectorie e getTaxisLocalização

--- routes/taxi.js
+++ routes/taxi.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getTaxis, getTaxisIdTrajectorie } = require('../controller/taxi');
+const { getTaxis, getTaxisIdTrajectorie, getTaxisLocalizacao } = require('../controller/taxi');
 
 
 
 // listando as rotas 
 
 router.get('/taxis', getTaxis);
-router.get('/taxis/:id/trajectories/:date', getTaxisIdTrajectorie)
+router.get('/taxis/:id/trajectories/:date', getTaxisIdTrajectorie);
+router.get('/taxis/trajectories/last', getTaxisLocalizacao);
+
 
 // Finalmente, exportamos o objeto router para que possa ser usado em outros arquivos
 module.exports = router;
